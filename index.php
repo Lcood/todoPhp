ea2dd7e74b22a068e7e4a935c8512e4a06e2ec9a
Lcood <fatumbd@protonmail.com>
2024-05-08 17:02:59 +0200

    base todo

---
+++ index.php
@@ -0,0 +1,64 @@
+
+<?php  
+  
+  require_once __DIR__. "/templates/header.php"; 
+
+?>
+
+    <!-- centered hero -->
+    <section class="px-4 py-5 my-5 text-center">
+      <img class="d-block mx-auto mb-4" src="./assets/images/logo.png" alt="" width="250" >
+      <div class="col-lg-6 mx-auto">
+        <p class="lead mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, dolores! Veritatis at reprehenderit dolor officia repudiandae velit recusandae dolores? Cumque recusandae harum vel eaque earum omnis quidem illo ea itaque.
+        Ex accusamus vel sapiente aliquam ut optio quaerat perspiciatis, possimus ducimus doloremque debitis quae hic? Et quibusdam doloremque deleniti molestiae in repudiandae voluptates quasi, accusamus aliquid alias qui asperiores modi.</p>
+        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+          <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
+        </div>
+      </div>
+    </section>
+
+    <!-- cards -->
+
+    <section class="container">
+      <div class="row text-center">
+        <div class="col-md-4 my-2">
+              <div class="card w-100">
+              <div class="card-body">
+                <h5 class="card-title">title</h5>
+                <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque, inventore maiores reiciendis officia harum ipsam, nobis labore autem vel, maxime ratione eligendi et atque ut pariatur quos nemo esse culpa?</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 my-2">
+          <div class="card w-100">
+              <div class="card-body">
+                <h5 class="card-title">title</h5>
+                <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque, inventore maiores reiciendis officia harum ipsam, nobis labore autem vel, maxime ratione eligendi et atque ut pariatur quos nemo esse culpa?</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+          </div>
+        </div>
+
+        <div class="col-md-4 my-2">
+          <div class="card w-100">
+              <div class="card-body">
+                <h5 class="card-title">title</h5>
+                <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque, inventore maiores reiciendis officia harum ipsam, nobis labore autem vel, maxime ratione eligendi et atque ut pariatur quos nemo esse culpa?</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+          </div>
+        </div>
+
+      </div>
+
+    </section>
+
+
+  
+
+<?php  
+  require_once __DIR__. "/templates/footer.php"; 
+
+?>
---
+++ templates/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Todo en Php et bootstrap</title>
+  <!-- bootstraps-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <!-- styles -->
+ <link rel="stylesheet" href="./assets/style.css">
+</head>
+<body>
+ <!-- navbar-->
+  <header class="container">
+    <nav class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+      <div class="col-md-3 mb-2 mb-md-0">
+        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+          <img src="./assets/images/logo.png" alt="logo" width="80">
+        </a>
+      </div>
+
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+        <li><a href="#" class="nav-link px-2">Features</a></li>
+        <li><a href="#" class="nav-link px-2">Pricing</a></li>
+        <li><a href="#" class="nav-link px-2">FAQs</a></li>
+        <li><a href="#" class="nav-link px-2">About</a></li>
+      </ul>
+
+      <div class="col-md-3 text-end">
+        <button type="button" class="btn btn-outline-primary me-2">Login</button>
+        <button type="button" class="btn btn-primary">Sign-up</button>
+      </div>
+    </nav>
+  </header>
+ <!-- End navbar-->
+ <!-- main-->
+
+  <main>
